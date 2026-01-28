8731d46cb1e5fd8fd970db4f746a3f924857a445
Utsav Lankapati <matrix.utsav.lankapati@gmail.com>
2026-01-28 15:07:49 +0530

    testing new solution

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,12 @@
-import { RouterProvider } from "react-router-dom";
-import router from "./Router/Router";
-import Layout from "./components/Layout/Layout";
+import { RouterProvider } from "react-router-dom"
+import router from "./Router/Router"
+import Layout from "./components/Layout/Layout"
 
 export default function App() {
-    return (
-        <Layout>
-            <RouterProvider router={router} />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
+  )
 }
+
