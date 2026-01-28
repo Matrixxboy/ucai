850b20e0251f5ede9f174cbe2174927cc267fd8d
Utsav Lankapati <matrix.utsav.lankapati@gmail.com>
2026-01-28 12:53:38 +0530

    ..

--- Server/Utils/paths.py
+++ Server/Utils/paths.py
@@ -1,8 +1,8 @@
 import os
 
-# Define global data directory: ~/.ucai
+# Define global data directory: ~/.ucai or from environment
 USER_HOME = os.path.expanduser("~")
-UCAI_BASE_DIR = os.path.join(USER_HOME, ".ucai")
+UCAI_BASE_DIR = os.getenv("UCAI_BASE_DIR", os.path.join(USER_HOME, ".ucai"))
 MODELS_DIR = os.path.join(UCAI_BASE_DIR, "models")
 MEMORY_DIR = os.path.join(UCAI_BASE_DIR, "memory")
 SETTINGS_FILE = os.path.join(UCAI_BASE_DIR, "settings.json")
