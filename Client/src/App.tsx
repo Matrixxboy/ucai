1a6a7f1c45643358b55a15f09214327451204e69
Utsav Lankapati <matrix.utsav.lankapati@gmail.com>
2026-01-28 15:13:53 +0530

    change the route method

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,19 @@
-import { RouterProvider } from "react-router-dom"
-import router from "./Router/Router"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Chatpage from "./pages/Chatpage"
+import Home from "./pages/Home"
+import Settings from "./pages/Settings"
 import Layout from "./components/Layout/Layout"
 
 export default function App() {
   return (
     <Layout>
-      <RouterProvider router={router} />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/chat" element={<Chatpage />} />
+                <Route path="/settings" element={<Settings />} />
+            </Routes>
+        </Router>
     </Layout>
   )
 }
